3f41050caef365e3148ed04154f67e7f578d17d0
Mezran <staylorallen@gmail.com>
2023-04-14 18:48:47 -0400

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Stephen Allen on 4/14/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
